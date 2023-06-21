125fd940206755f8e9d2d74958bf0c5867099cbf
lizhanyang505 <596114632@qq.com>
2023-06-21 12:06:11 +0800

    four_works_1_8

---
+++ pallets/kitties/src/mock.rs
@@ -0,0 +1,99 @@
+use crate as pallet_kitties;
+use frame_support::{
+	parameter_types,
+	traits::{ConstU128, ConstU16, ConstU32, ConstU64},
+	PalletId,
+};
+use sp_core::H256;
+use sp_runtime::{
+	testing::Header,
+	traits::{BlakeTwo256, IdentityLookup},
+};
+
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
+type Block = frame_system::mocking::MockBlock<Test>;
+
+// Configure a mock runtime to test the pallet.
+frame_support::construct_runtime!(
+	pub enum Test where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic,
+	{
+		System: frame_system,
+		Balances: pallet_balances,
+		InsecureRandomnessCollectiveFlip: pallet_insecure_randomness_collective_flip,
+		KittiesModule: pallet_kitties,
+	}
+);
+
+// frame-system
+pub type AccountId = u64;
+impl frame_system::Config for Test {
+	type BaseCallFilter = frame_support::traits::Everything;
+	type BlockWeights = ();
+	type BlockLength = ();
+	type DbWeight = ();
+	type RuntimeOrigin = RuntimeOrigin;
+	type RuntimeCall = RuntimeCall;
+	type Index = u64;
+	type BlockNumber = u64;
+	type Hash = H256;
+	type Hashing = BlakeTwo256;
+	type AccountId = AccountId;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type Header = Header;
+	type RuntimeEvent = RuntimeEvent;
+	type BlockHashCount = ConstU64<250>;
+	type Version = ();
+	type PalletInfo = PalletInfo;
+	type AccountData = pallet_balances::AccountData<Balance>;
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type SystemWeightInfo = ();
+	type SS58Prefix = ConstU16<42>;
+	type OnSetCode = ();
+	type MaxConsumers = ConstU32<16>;
+}
+
+// pallet-balances
+pub type Balance = u128;
+pub const EXISTENTIAL_DEPOSIT: Balance = 500;
+impl pallet_balances::Config for Test {
+	type MaxLocks = ConstU32<50>;
+	type MaxReserves = ();
+	type ReserveIdentifier = [u8; 8];
+	type Balance = Balance;
+	type RuntimeEvent = RuntimeEvent;
+	type DustRemoval = ();
+	type ExistentialDeposit = ConstU128<EXISTENTIAL_DEPOSIT>;
+	type AccountStore = System;
+	type WeightInfo = pallet_balances::weights::SubstrateWeight<Test>;
+}
+
+// pallet-insecure-randomness-collective-flip
+impl pallet_insecure_randomness_collective_flip::Config for Test {}
+
+// curent pallet
+parameter_types! {
+	pub KittiesPalletId: PalletId = PalletId(*b"py/kitty");
+	pub KittyPrice: Balance = EXISTENTIAL_DEPOSIT * 1000;
+}
+impl pallet_kitties::Config for Test {
+	type RuntimeEvent = RuntimeEvent;
+	type PalletId = KittiesPalletId;
+	type Currency = Balances;
+	type KittyDnaRandomness = InsecureRandomnessCollectiveFlip;
+	type KittyPrice = KittyPrice;
+}
+
+// Build genesis storage according to the mock runtime.
+pub fn new_test_ext() -> sp_io::TestExternalities {
+	let mut ext: sp_io::TestExternalities =
+		frame_system::GenesisConfig::default().build_storage::<Test>().unwrap().into();
+	ext.execute_with(|| {
+		// Go past genesis block so events get deposited
+		System::set_block_number(1);
+	});
+	ext
+}
