6be8da859edc9c120dcb0d5ee9b770752647d405
lizhanyang505 <596114632@qq.com>
2023-06-29 15:05:04 +0800

    add trait erc20

--- lib.rs
+++ lib.rs
@@ -3,6 +3,7 @@
 #[ink::contract]
 mod erc20 {
     use ink::storage::Mapping;
+    use trait_erc20::{TERC20, Error, Result};
     /// Defines the storage of your contract.
     /// Add new fields to the below struct in order
     /// to add new static storage fields to your contract.
@@ -35,14 +36,8 @@ mod erc20 {
         value: Balance,
     }
 
-    #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
-    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
-    pub enum Error {
-        BalanceTooLow,
-        AllowanceTooLow,
-    }
+  
 
-    type Result<T> = core::result::Result<T, Error>;
 
     impl Erc20 {
         /// Constructor that initializes the `bool` value to the given `init_value`.
@@ -68,50 +63,7 @@ mod erc20 {
             }
         }
 
-        #[ink(message)] // 只读不可变
-        pub fn total_supply(&self) -> Balance {
-            self.total_supply
-        }
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
-        #[ink(message)]
-        pub fn balance_of(&self, who: AccountId) -> Balance {
-            self.balances.get(&who).unwrap_or_default()
-        }
-        #[ink(message)]
-        pub fn transfer(&mut self, to: AccountId, value: Balance) -> Result<()> {
-            let sender  = self.env().caller();
-            return self.transfer_helper(&sender, &to, value);
-        }
-
-        #[ink(message)]
-        pub fn transfer_from(&mut self, from: AccountId, to: AccountId, value: Balance) -> Result<()> {
-            let sender = self.env().caller();
-            let mut allowance = self.allowance.get(&(from, sender)).unwrap_or_default();
-
-            if allowance < value {
-                return Err(Error::AllowanceTooLow);
-            }
-
-            self.allowance.insert(&(from, sender), &(allowance - value));
-            return self.transfer_helper(&from, &to, value);
-        }
-
-        #[ink(message)]
-        pub fn approve(&mut self, to: AccountId, value:Balance) -> Result<()> {
-            let sender = self.env().caller();
-            self.allowance.insert(&(sender, to), &value);
-
-            self.env().emit_event(Approval {
-                from: sender,
-                to,
-                value
-            });
-
-        Ok(())
-
-        }
+       
 
         pub fn transfer_helper(&mut self, from: &AccountId, to: &AccountId, value: Balance) -> Result<()> {
             let balance_from = self.balance_of(*from);
@@ -125,25 +77,70 @@ mod erc20 {
 
             self.env().emit_event(
                 Transfer {
-                    from: *from,
-                    to: *to,
+                    from: Some(*from),
+                    to: Some(*to),
                     value
                 }
             );
 
             Ok(())
         }
-    
     }
+
+        impl TERC20 for Erc20 {
+            #[ink(message)] // 只读不可变
+             fn total_supply(&self) -> Balance {
+                self.total_supply
+            }
+            /// A message that can be called on instantiated contracts.
+            /// This one flips the value of the stored `bool` from `true`
+            /// to `false` and vice versa.
+            #[ink(message)]
+             fn balance_of(&self, who: AccountId) -> Balance {
+                self.balances.get(&who).unwrap_or_default()
+            }
+            #[ink(message)]
+             fn transfer(&mut self, to: AccountId, value: Balance) -> Result<()> {
+                let sender  = self.env().caller();
+                return self.transfer_helper(&sender, &to, value);
+            }
+    
+            #[ink(message)]
+             fn transfer_from(&mut self, from: AccountId, to: AccountId, value: Balance) -> Result<()> {
+                let sender = self.env().caller();
+                let mut allowance = self.allowance.get(&(from, sender)).unwrap_or_default();
+    
+                if allowance < value {
+                    return Err(Error::AllowanceTooLow);
+                }
+    
+                self.allowance.insert(&(from, sender), &(allowance - value));
+                return self.transfer_helper(&from, &to, value);
+            }
+    
+            #[ink(message)]
+             fn approve(&mut self, to: AccountId, value:Balance) -> Result<()> {
+                let sender = self.env().caller();
+                self.allowance.insert(&(sender, to), &value);
+    
+                self.env().emit_event(Approval {
+                    from: sender,
+                    to,
+                    value
+                });
+    
+            Ok(())
+    
+            }
+        }
+    
+    
 
     #[cfg(test)]
     mod tests {
-        use ink_e2e::subxt::ext::sp_runtime::serde::de::value;
-        use scale::Decode;
 
         use super::*;
-        type Event = <Erc20 as ink::reflect::ContractEventBase>::Type;
-
+        type Event =  <Erc20 as ink::reflect::ContractEventBase>::Type;
         #[ink::test]
         fn constructor_works() {
             let erc20 = Erc20::new(10000);
@@ -151,21 +148,59 @@ mod erc20 {
             assert_eq!(erc20.total_supply(), 10000);
             assert_eq!(erc20.balance_of(accounts.alice), 10000);
 
-            let emited_events = ink::env::test::recorded_events().collect::<Vec<_>>();
+            let emitted_events = ink::env::test::recorded_events().collect::<Vec<_>>();
 
-            let event = &emited_events[0];
+            let event = &emitted_events[0];
 
-            let decoded = <Event as scale::Decode>::decode(&mut &event[..]).expect("decoded error");
+            let decoded = <Event as scale::Decode>::decode(&mut &event.data[..]).expect("decoded error");
 
             match decoded {
-                Event::Transfer(Transfer(from, to, value)) => {
-                    assert_eq!(from.is_none(), "nint for error");
+                Event::Transfer(Transfer{from, to, value}) => {
+                    assert_eq!(from.is_none(), true);
                     assert_eq!(to, Some(accounts.alice), "nint to error");
-                    
-                }
+                    assert_eq!(value, 10000, "nint value error");
+                },
+                _ => panic!("match for error")
             }
 
         }   
+
+        #[ink::test]
+        fn transfer_should_work() {
+            let mut erc20 = Erc20::new(10000);
+            let accounts = ink::env::test::default_accounts::<ink::env::DefaultEnvironment>();
+            let res = erc20.transfer(accounts.bob, 12);
+
+            assert!(res.is_ok());
+            //assert!(erc20.balance_of(accounts.alice), 10000_12);
+            assert_eq!(erc20.balance_of(accounts.bob), 12);
+        }
+
+        #[cfg(feature = "e2e-tests")]
+        mod e2e_tests{
+            use super::*;
+            type Event =  <Erc20 as ink::reflect::ContractEventBase>::Type;
+
+            #[ink_e2e::test]
+            async fn e2e_transfer(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
+                let total_supply = 123;
+                let constructor = Erc20Ref::new(total_supply);
+                let contract_acc_id = client.instantiated(
+                    "erc20",
+                    &ink_e2e,
+                    constractor,
+                    0,
+                    None
+                ).await
+                .expect("instantiate faild")
+                .account_id;
+
+            let alice_acc  = ink_e2e::account_id(ink_e2e::AccountKeyring::Alice);
+            let bob_acc = ink_e2e::account_id(ink_e2e::AccountKeyring::Bob);
+
+            }
+        
+        }
 
     }
 
