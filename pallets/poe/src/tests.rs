db79cb3e5fcab46afa5b43b8e4c4e2f12a0b0726
lizhanyang505 <596114632@qq.com>
2023-07-01 11:15:00 +0800

    six_works

--- pallets/poe/src/tests.rs
+++ pallets/poe/src/tests.rs
@@ -1,6 +1,18 @@
 use super::*;
 use crate::{mock::*, Error};
 use frame_support::{assert_noop, assert_ok, BoundedVec};
+
+
+const ACCOUNT_ID_1: u64 = 1;
+const ACCOUNT_ID_2: u64 = 2;
+const ACCOUNT_ID_3: u64 = 3;
+
+fn new_claim() -> BoundedVec<u8, ConstU32<10>> {
+	return BoundedVec::try_from(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).unwrap()
+}
+
+
+
 #[test]
 fn create_claim_works() {
 
@@ -10,7 +22,7 @@ new_test_ext().execute_with(|| {
 
    let bound_claim = BoundedVec::try_from(vec![0,1]).unwrap();
 
-    assert_ok!(PoeModule::create_claim(RuntimeOrigin::signed(1), claim.clone()));
+    assert_ok!(PoeModule::create_claim(RuntimeOrigin::signed(1), bound_claim.clone()));
     
   
     assert_eq!(
@@ -24,11 +36,12 @@ new_test_ext().execute_with(|| {
 fn create_claim_failed_when_claim_already_exist() {
     new_test_ext().execute_with(|| {
        // let bound_claim = BoundedVec::try_from(vec![0, 1]).unwrap();
-        let claim = vec![0, 1];
-        let _ = PoeModule::create_claim(RuntimeOrigin::signed(1), claim.clone());
+        let bound_claim: BoundedVec<u8, ConstU32<10>> = BoundedVec::try_from(vec![0,1]).unwrap();
+
+        let _ = PoeModule::create_claim(RuntimeOrigin::signed(1), bound_claim.clone());
 
         assert_noop!(
-            PoeModule::create_claim(RuntimeOrigin::signed(1), claim.clone()),
+            PoeModule::create_claim(RuntimeOrigin::signed(1), bound_claim.clone()),
             Error::<Test>::ProofAlreadyExist
         );
 
@@ -38,11 +51,11 @@ fn create_claim_failed_when_claim_already_exist() {
 #[test]
 fn revoke_claim_works() {
     new_test_ext().execute_with(|| {
-        let claim = vec![0, 1];
+        let bound_claim: BoundedVec<u8, ConstU32<10>> = BoundedVec::try_from(vec![0,1]).unwrap();
 
-        let _ = PoeModule::create_claim(RuntimeOrigin::signed(1), claim.clone());
+        let _ = PoeModule::create_claim(RuntimeOrigin::signed(1), bound_claim.clone());
 
-        assert_ok!(PoeModule::revoke_claim(RuntimeOrigin::signed(1), claim.clone()));
+        assert_ok!(PoeModule::revoke_claim(RuntimeOrigin::signed(1), bound_claim.clone()));
     })
     
 }
@@ -50,9 +63,9 @@ fn revoke_claim_works() {
 #[test]
 fn revoke_claim_faild_when_claim_not_exist() {
     new_test_ext().execute_with(|| {
-        let claim = vec![0, 1];
+        let bound_claim: BoundedVec<u8, ConstU32<10>> = BoundedVec::try_from(vec![0,1]).unwrap();
         assert_noop!(
-            PoeModule::revoke_claim(RuntimeOrigin::signed(1), claim.clone()),
+            PoeModule::revoke_claim(RuntimeOrigin::signed(1), bound_claim.clone()),
             Error::<Test>::ClaimNotExist
         );
     })
@@ -61,14 +74,82 @@ fn revoke_claim_faild_when_claim_not_exist() {
 #[test]
 fn revoke_claim_faild_with_wrong_owner() {
     new_test_ext().execute_with(|| {
-        let claim = vec![0, 1];
+        let bound_claim: BoundedVec<u8, ConstU32<10>> = BoundedVec::try_from(vec![0,1]).unwrap();
         let _ = PoeModule::create_claim(RuntimeOrigin::signed(1), claim.clone());
 
         assert_noop!(
-            PoeModule::revoke_claim(RuntimeOrigin::signed(2), claim.clone()),
+            PoeModule::revoke_claim(RuntimeOrigin::signed(2), bound_claim.clone()),
             Error::<Test>::NotClaimOwner
         );
 
     })
+}
+
+#[test]
+fn transfer_claim_works() {
+	new_test_ext().execute_with(|| {
+		let claim = new_claim();
+		let signer = RuntimeOrigin::signed(ACCOUNT_ID_1);
+
+		// 创建存证
+		assert_ok!(PalletPoe::create_claim(signer.clone(), claim.clone()));
+		// 转移存证
+		assert_ok!(PalletPoe::transfer_claim(signer, ACCOUNT_ID_2, claim.clone()));
+		// 检查存证
+		assert_eq!(PalletPoe::proofs(&claim), Some((ACCOUNT_ID_2, System::block_number())));
+	})
+}
+
+#[test]
+fn transfer_claim_failed_when_claim_not_exist() {
+	new_test_ext().execute_with(|| {
+		let claim = new_claim();
+		let signer = RuntimeOrigin::signed(ACCOUNT_ID_1);
+
+		// 转移存证
+		assert_noop!(
+			PalletPoe::transfer_claim(signer, ACCOUNT_ID_2, claim.clone()),
+			Error::<Test>::ClaimNotExist
+		);
+		// 检查存证
+		assert_eq!(PalletPoe::proofs(&claim), None);
+	})
+}
+
+#[test]
+fn transfer_claim_failed_when_not_owner() {
+	new_test_ext().execute_with(|| {
+		let claim = new_claim();
+		let signer = RuntimeOrigin::signed(ACCOUNT_ID_1);
+		let signer_2 = RuntimeOrigin::signed(ACCOUNT_ID_2);
+
+		// 创建存证
+		assert_ok!(PalletPoe::create_claim(signer, claim.clone()));
+		// 转移存证
+		assert_noop!(
+			PalletPoe::transfer_claim(signer_2, ACCOUNT_ID_3, claim.clone()),
+			Error::<Test>::NotClaimOwner
+		);
+		// 检查存证
+		assert_eq!(PalletPoe::proofs(&claim), Some((ACCOUNT_ID_1, System::block_number())));
+	})
+}
+
+#[test]
+fn transfer_claim_failed_when_transfer_to_owner() {
+	new_test_ext().execute_with(|| {
+		let claim = new_claim();
+		let signer = RuntimeOrigin::signed(ACCOUNT_ID_1);
+
+		// 创建存证
+		assert_ok!(PalletPoe::create_claim(signer.clone(), claim.clone()));
+		// 转移存证
+		assert_noop!(
+			PalletPoe::transfer_claim(signer, ACCOUNT_ID_1, claim.clone()),
+			Error::<Test>::TransferToOwner
+		);
+		// 检查存证
+		assert_eq!(PalletPoe::proofs(&claim), Some((ACCOUNT_ID_1, System::block_number())));
+	})
 }
 
